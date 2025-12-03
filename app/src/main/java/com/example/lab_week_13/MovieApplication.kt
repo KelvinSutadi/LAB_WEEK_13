7446586d609ba603002ad81f58ded7f26090a361
KelvinSutadi <kelvinsutadi06@gmail.com>
2025-12-03 16:34:42 +0700

    Commit No. 3 – update to WorkManager

--- app/src/main/java/com/example/lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/lab_week_13/MovieApplication.kt
@@ -1,10 +1,15 @@
 package com.example.lab_week_13
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.example.lab_week_13.api.MovieService
 import com.example.lab_week_13.database.MovieDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MovieApplication : Application() {
 
@@ -12,6 +17,7 @@ class MovieApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(MoshiConverterFactory.create())
@@ -20,5 +26,18 @@ class MovieApplication : Application() {
         val movieService = retrofit.create(MovieService::class.java)
         val movieDatabase = MovieDatabase.getInstance(applicationContext)
         movieRepository = MovieRepository(movieService, movieDatabase)
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val workRequest = PeriodicWorkRequest.Builder(
+            MovieWorker::class.java,
+            1, TimeUnit.HOURS // Interval 1 Jam
+        ).setConstraints(constraints)
+            .addTag("movie-work")
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueue(workRequest)
     }
 }
--- app/src/main/java/com/example/lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_13
 
+import android.util.Log
 import com.example.lab_week_13.api.MovieService
 import com.example.lab_week_13.database.MovieDatabase
 import com.example.lab_week_13.model.Movie
@@ -30,5 +31,20 @@ class MovieRepository(
                 emit(savedMovies)
             }
         }.flowOn(Dispatchers.IO)
+    }
+
+    suspend fun fetchMoviesFromNetwork() {
+        val movieDao = movieDatabase.movieDao()
+        try {
+            val apiKey = "4bf75ebecbd25b57f0c8485e3b61c04e"
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            val moviesFetched = popularMovies.results
+
+            movieDao.addMovies(moviesFetched)
+
+            Log.d("MovieRepository", "Success fetching data from network")
+        } catch (exception: Exception) {
+            Log.d("MovieRepository", "An error occurred: ${exception.message}")
+        }
     }
 }
